2093a61a3ceb6708a4ba345df057f6fad927d292
okwiridesigns <okwirioduor@gmail.com>
2025-04-28 13:01:32 +0100

    completed home page

--- laab/src/App.js
+++ laab/src/App.js
@@ -5,13 +5,12 @@ import Navbar from './navbar';
 function App() {
   return (
     <>
+      <div className='navigation'>
+          <Navbar/>
+      </div>
       <div>
           <Home/>
-          </div>
-          
-          <div>
-          <Navbar/>
-          </div>
+      </div>
     </>
     
   );
--- laab/src/home.js
+++ laab/src/home.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import wavy_line from './images/wavy_yellow_line.png'
+import wavy_line from './images/wavy_yellow_line.png';
+import homeImg from './images/home_img.png';
+import redStar from './images/red_star.png';
+import heartDots from './images/heart_dots.png';
 
 
 
@@ -9,6 +12,15 @@ function Home() {
       <>
               <div >
               <img className="wavyLine" src={wavy_line} alt=""/>
+              </div>
+              <div >
+              <img className="home_img" src={homeImg} alt=""/>
+              </div>
+              <div >
+              <img className="red_star" src={redStar} alt=""/>
+              </div>
+              <div >
+              <img className="heart_dots" src={heartDots} alt=""/>
               </div>
               <div className='main-title'>LAAB!</div>
               <div className='body-txt' id='homeTxt'>
