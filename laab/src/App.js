b9518aeef9a5750c6e60dc9259d221eece588126
okwiridesigns <okwirioduor@gmail.com>
2025-06-18 13:50:28 +0100

    added usestate to app.js

--- laab/src/App.js
+++ laab/src/App.js
@@ -6,41 +6,29 @@ import OurStory from './OurStory';
 import Change from './change';
 import Footer from './footer';
 import Contact from './contact_form';
-
+import ReadMore from './read_more';
+import { useState } from 'react';
 
 function App() {
+  const [showReadMore, setShowReadMore] = useState(false);
+
   return (
     <>
-      <div>
-          < Navbar/>
-      </div>
-      
-      <div>
-          <Home/>
-      </div>
-      
-      <div> 
-        <OurStory/>
-      </div>
+      <Navbar />
 
-      <div> 
-        <Faqs/>
-      </div>
-
-      <div> 
-        <Change/>
-      </div>
-
-      <div> 
-        <Contact/>
-      </div>
-
-       <div> 
-        <Footer/>
-      </div>
-
+      {showReadMore ? (
+        <ReadMore onBack={() => setShowReadMore(false)} />
+      ) : (
+        <>
+          <Home />
+          <OurStory onReadMoreClick={() => setShowReadMore(true)} />
+          <Faqs />
+          <Change />
+          <Contact />
+          <Footer />
+        </>
+      )}
     </>
-    
   );
 }
 
