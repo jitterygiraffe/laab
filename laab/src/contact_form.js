2368a08c9a174fad37fa87343f0df6652f449764
okwiridesigns <okwirioduor@gmail.com>
2025-06-19 14:46:39 +0100

    minor content adjustments to contact form

--- laab/src/contact_form.js
+++ laab/src/contact_form.js
@@ -3,44 +3,54 @@ import './contact.css';
 
 const ContactForm = () => {
   return (
+    <div id="contact-us">
+      <section className="contact-container">
+        <div className="contact-left">
+          <h2 id="contact-title">Let's Chat</h2>
+          <p id="contact-txt">
+            This project is designed to engage the community 
+            around it, to build and strengthen
+            individual community members’ sense of 
+            ownership and participation in the care 
+            of the playground.
+          </p>
+          <p id="contact-txt">
+            For this reason, we are always thrilled to 
+            hear from our community members! Don’t 
+            hesitate to reach out to us in case of any 
+            question, suggestion, or words of encouragement.
+            We truly look forward to hearing from you!
+          </p>
+        </div>
 
-      <div id="contact-us">
-      
-        <section className="contact-container">
-      <div className="contact-left">
-        <h2 id="contact-title">Let's Chat</h2>
-        <p id="contact-txt">
-          This project is designed to engage the 
-          community around it, to build and strengthen 
-          individual community members’ sense of 
-          ownership and participation in the care of the playground. 
-        </p>
-        <p id="contact-txt">
-          For this reason, we are always excited to hear from 
-          our community members! Whether you have a question, 
-          suggestion, or food for thought, please don't hesitate 
-          to reach out to us.
-        </p>
-      </div>
+        <div className="contact-right">
+          <form
+            name="contact"
+            method="POST"
+            data-netlify="true"
+            netlify-honeypot="bot-field"
+            className="contact-form"
+          >
+            
+            <input type="hidden" name="form-name" value="contact" />
+            <p hidden>
+              <label>
+                Don’t fill this out if you're human: <input name="bot-field" />
+              </label>
+            </p>
 
-      <div className="contact-right">
-        
-        <form className="contact-form">
-          <input type="text" placeholder="Name *" required />
-          <input type="email" placeholder="Email *" required />
-          <input type="text" placeholder="Company" />
-          <input type="text" placeholder="Phone" />
-          <textarea placeholder="Message" rows="5"></textarea>
-          <button id='contact-btn' type="submit">Contact Us</button>
-        </form>
-      </div>
-    </section>
-
-
-      </div>
-
-
-      );
+            
+            <input type="text" name="name" placeholder="Name *" required />
+            <input type="email" name="email" placeholder="Email *" required />
+            <input type="text" name="company" placeholder="Company" />
+            <input type="text" name="phone" placeholder="Phone" />
+            <textarea name="message" placeholder="Message" rows="5"></textarea>
+            <button id="contact-btn" type="submit">Contact Us</button>
+          </form>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default ContactForm;
