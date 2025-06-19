ea92a60296ebf3261fbecbcfb7c101e96b549bc5
okwiridesigns <okwirioduor@gmail.com>
2025-06-19 17:12:02 +0100

    added gofund me link to donate button in navbar

--- laab/src/Navbar.js
+++ laab/src/Navbar.js
@@ -14,7 +14,11 @@ function Navbar() {
         <a href="#our-story">Our Story</a>
         <a href="#faqs">FAQs</a>
         <a href="#contact-us">Contact Us</a>
-        <a href="#donate" id="donate-btn">Donate Now</a>
+        <a href="https://www.gofundme.com/f/help-build-the-new-sidi-bou-said-playground?lang=en_US&utm_campaign=man_sharesheet_dash&utm_content=amp10_t1-amp13_c-amp14_t2&utm_medium=customer&utm_source=copy_link&attribution_id=sl%3Ab976d453-7e48-4b13-96b7-0a89310ac81e" 
+           id="donate-btn"
+           target="_blank" 
+           rel="noopener noreferrer"
+           >Donate Now</a>
         <button className="lang-toggle" onClick={toggleLanguage}>
           {language}
         </button>
