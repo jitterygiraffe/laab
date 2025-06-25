6dc3035a936b1e53af0d6d820cd26e1a1dd6c8d0
okwiridesigns <okwirioduor@gmail.com>
2025-06-25 23:19:26 +0100

    added asset for missing section

--- laab/src/features_section.js
+++ laab/src/features_section.js
@@ -46,6 +46,18 @@ const features = [
   
 ];
 
+const htmlCode = `
+        <div style="position: relative; width: 100%; height: 0; padding-top: 56.2500%;
+ padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
+ border-radius: 8px; will-change: transform;">
+  <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
+    src="https://www.canva.com/design/DAGrXQOl0UQ/LStCk8eVYpK8Gn37opf7HA/watch?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+  </iframe>
+</div>
+<a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGrXQOl0UQ&#x2F;LStCk8eVYpK8Gn37opf7HA&#x2F;watch?utm_content=DAGrXQOl0UQ&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">laab_teaser</a> by churu puka
+      `;
+
+
 const FeaturesSection = () => {
   return (
     <>
@@ -64,7 +76,12 @@ const FeaturesSection = () => {
           </div>
           <p className="feature-text">{feature.text}</p>
           {index < features.length - 1 && <div className="divider" />}
+
+          <div dangerouslySetInnerHTML={{ __html: htmlCode }} />
           </div>
+
+         
+          
       ))}
     </section>
     </>
